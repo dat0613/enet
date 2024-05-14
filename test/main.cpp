ce9067d86381d3a379c180473b47d2cde5f637ee
dat0613 <dat0613@naver.com>
2024-05-14 19:00:11 +0900

    Build: 테스트용 프로젝트 추가

---
+++ test/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include <enet/enet.h>
+
+int main()
+{
+	if (enet_initialize() != 0)
+	{
+		fprintf(stderr, "An error occurred while initializing ENet.\n");
+		return EXIT_FAILURE;
+	}
+	atexit(enet_deinitialize);
+
+	ENetAddress address;
+	ENetHost* server;
+
+	address.host = ENET_HOST_ANY;
+	address.port = 1234;
+
+	server = enet_host_create(&address, 32, 2, 0, 0);
+
+	if (nullptr == server)
+	{
+		fprintf(stderr,
+			"An error occurred while trying to create an ENet server host.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	ENetHost* client;
+
+	client = enet_host_create(NULL /* create a client host */,
+		1 /* only allow 1 outgoing connection */,
+		2 /* allow up 2 channels to be used, 0 and 1 */,
+		0 /* assume any amount of incoming bandwidth */,
+		0 /* assume any amount of outgoing bandwidth */);
+
+	if (client == NULL)
+	{
+		fprintf(stderr,
+			"An error occurred while trying to create an ENet client host.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	enet_host_destroy(server);
+	enet_host_destroy(client);
+}
